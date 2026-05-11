e489c539707b380aadf9dbfb7ec90a2ae1ead262
SM300000 <sanaamatallah112@gmail.com>
2026-05-11 08:35:40 +0200

    r

--- menu.php
+++ menu.php
@@ -2,14 +2,14 @@
   <h1>Mon Portfolio</h1>
   <nav>
     <ul>
-      <li><a href="index.html">Accueil</a></li>
-      <li><a href="presentation.html">Présentation</a></li>
-      <li><a href="cv.html">CV</a></li>
-      <li><a href="projets.html">Projets</a></li>
-      <li><a href="stages.html">Stages</a></li>
-      <li><a href="situations.html">Situations</a></li>
-      <li><a href="veille.html">Veille</a></li>
-      <li><a href="contact.html">Contact</a></li>
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="presentation.php">Présentation</a></li>
+      <li><a href="cv.php">CV</a></li>
+      <li><a href="projets.php">Projets</a></li>
+      <li><a href="stages.php">Stages</a></li>
+      <li><a href="situations.php">Situations</a></li>
+      <li><a href="veille.php">Veille</a></li>
+      <li><a href="contact.php">Contact</a></li>
     </ul>
   </nav>
 </header>
